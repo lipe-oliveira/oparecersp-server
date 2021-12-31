fe9c485efdb80b6c1f37a230eedfeb726cba506d
lipe.oliveira <felipeoliveira.b@gmail.com>
2021-12-31 12:14:58 -0300

    auto

--- src/app/models/blog.js
+++ src/app/models/blog.js
@@ -37,7 +37,7 @@ const schema = new mongoose.Schema({
     }
 });
 
-await schema.plugin(mongoose_paginate);
+schema.plugin(mongoose_paginate);
 
 const Blog = mongoose.model("Blog", schema);
 module.exports = Blog;
